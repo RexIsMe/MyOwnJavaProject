466d6e6c8fd145939708cebbd973c6e9d04931a2
rexisme <996867328@qq.com>
2019-07-22 15:13:48 +0800

    daily

---
+++ hadoop/HDFSClient/src/main/java/com/atguigu/hdfs/HdfsClient.java
@@ -0,0 +1,26 @@
+package com.atguigu.hdfs;
+
+/**
+ * @author Rex
+ * @title: HdfsClient
+ * @projectName HDFSclient
+ * @description: TODO
+ * @date 2019/7/2214:58
+ */
+public class HdfsClient {
+
+    public static void main(String[] arg0){
+
+    }
+
+
+    /**
+     * 测试用java调用hadoop创建一个目录
+     */
+    public void testMkdir(){
+
+
+
+    }
+
+}
