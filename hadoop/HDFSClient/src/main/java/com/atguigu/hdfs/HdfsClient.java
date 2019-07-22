cbbdf642dcad797b3ff83a997cb95756359e4165
rexisme <996867328@qq.com>
2019-07-22 16:15:30 +0800

    daily

--- hadoop/HDFSClient/src/main/java/com/atguigu/hdfs/HdfsClient.java
+++ hadoop/HDFSClient/src/main/java/com/atguigu/hdfs/HdfsClient.java
@@ -1,5 +1,14 @@
 package com.atguigu.hdfs;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  * @author Rex
  * @title: HdfsClient
@@ -9,14 +18,18 @@ package com.atguigu.hdfs;
  */
 public class HdfsClient {
 
-    public static void main(String[] arg0){
-
+    public static void main(String[] arg0) throws URISyntaxException, IOException, InterruptedException {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop102:9000"), conf, "atguigu");
+        fs.mkdirs(new Path("/0717/gaoxinban"));
+        fs.close();
     }
 
 
     /**
      * 测试用java调用hadoop创建一个目录
      */
+    @Test
     public void testMkdir(){
 
 
